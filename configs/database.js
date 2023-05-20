67f935d352adc28be7e7c842ec4209d4ce49f373
Rico Samuel <samuelricoss@yahoo.co.id>
2023-05-20 09:22:49 +0700

    configupdate

--- configs/database.js
+++ configs/database.js
@@ -1,24 +1,24 @@
-const mysql = require("mysql");
-const util = require("util");
-const dotenv = require("dotenv");
+const mysql = require('mysql');
+const util = require('util');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.BD_PASSWORD,
-	database: process.env.BD_NAME,
+  host: 'containers-us-west-135.railway.app',
+  user: 'root',
+  password: 'TvHw47w2nQF20RrlIRRN',
+  database: 'railway',
 });
 
 connection.query = util.promisify(connection.query).bind(connection);
 
 connection.connect(function (err) {
-	if (err) {
-		console.log("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected");
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected');
 });
 
 module.exports = connection;
